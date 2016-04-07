04b6e04765d511d3b7f0b1972a78447105f207c8
Josh Washke <jwashke@icloud.com>
2016-04-06 20:02:56 -0600

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    names = tags_string.split(",").map{|s| s.strip.downcase}.uniq
+    new_or_found_tags = names.map do |name|
+      Tag.find_or_create_by(name: name)
+    end
+    self.tags = new_or_found_tags
+  end
 end
